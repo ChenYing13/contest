e30509876d06207f33f47d0a0a2624c6eb6c8ef0
陈颖 <ChenY@ChenYingdeMacBook-Pro.local>
2019-03-11 18:53:31 +0800

    111

--- js-mocha/test/test_contest.js
+++ js-mocha/test/test_contest.js
@@ -2,7 +2,7 @@ var {expect} = require('chai');
 var {Contest} = require('../src/contest.js');
 describe("contest", function() {
 
-  it("shows a passed test", function() {
+  it("shows a passed test111", function() {
     const contest = new Contest();
     expect(contest.method()).to.equal(true);
   });
